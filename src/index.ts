5813a014cf00adb5d344fb791adb7d634fc80243
Gabriel Pretel <gabrilinex@gmail.com>
2024-07-30 14:23:13 +0200

    First seed

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "./css/styles.scss";
+
+const user = "John Doe";
+console.log(`Hello ${user}!`);
+console.log("This app is using Vite + TypeScript");
+console.log("Note: Change de remote url for GitHub!");
